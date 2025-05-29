ebdb4bc1a251ef3c539d0318cc902d382581e20d
Umut Can ERYILDIZ <uwsiletisim@gmail.com>
2025-05-29 17:19:03 +0300

    Implement UI Scripts

---
+++ Assets/_Scripts/Interfaces/IGameService.cs
@@ -0,0 +1,15 @@
+﻿using R3;
+
+namespace Interfaces
+{
+    public interface IGameService
+    {
+        ReactiveProperty<int> Timer { get; }
+
+        Subject<Unit> GameEnded { get; }
+        Subject<Unit> GameStarted { get; }
+
+        void EndGame();
+
+    }
+}
---
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Interfaces;
+using R3;
+using UnityEngine;
+
+namespace Managers
+{
+    public class GameManager : MonoBehaviour, IGameService
+    {
+        #region Fields
+
+        private bool _gameEnded;
+
+        private CancellationTokenSource _tokenSource;
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Start()
+        {
+            OnGameStart(_tokenSource.Token).Forget();
+        }
+
+        
+        private void OnDestroy()
+        {
+            _tokenSource?.Cancel();
+        }
+        #endregion
+
+        #region Private Methods
+
+        private async UniTask OnGameStart(CancellationToken token)
+        {
+            GameStarted.OnNext(Unit.Default);
+
+            while (!_gameEnded && !_tokenSource.Token.IsCancellationRequested)
+            {
+                try
+                {
+                    await UniTask.Delay(TimeSpan.FromSeconds(1), cancellationToken: token);
+                    Timer.Value++;
+                }
+                catch (OperationCanceledException e)
+                {
+                    //on cancel
+                    Debug.Log("Timer cancelled");
+                    break;
+                }
+            }
+        }
+
+        #endregion
+
+        #region IGameService Members
+
+        public ReactiveProperty<int> Timer { get; private set; } = new ReactiveProperty<int>(0);
+
+        public Subject<Unit> GameEnded { get; } = new Subject<Unit>();
+        public Subject<Unit> GameStarted { get; } = new Subject<Unit>();
+
+        public void EndGame()
+        {
+            _gameEnded = true;
+            _tokenSource?.Cancel();
+            GameEnded.OnNext(Unit.Default);
+        }
+
+        #endregion
+    }
+}
